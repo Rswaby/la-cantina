988ab40a097e11546dcd86d6f8de9c039579a6dc
Rohan Swaby <rohan_swaby51@yahoo.com>
2018-11-24 11:06:27 -0500

    create event in progress

---
+++ orange-aliens/src/containers/CreateEvent/CreateEvent.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+
+//import { Login } from '../../components';
+//import { AuthContext } from '../contexts/Auth.context';
+
+class CreateEvent extends Component {
+  //static contextType = AuthContext;
+
+  render() {
+    return (
+        <h4>Create Event Form</h4>
+    )
+  }
+}
+
+export default CreateEvent;
