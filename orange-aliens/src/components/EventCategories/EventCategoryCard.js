49f8b159e1b446b65019cfa9fce33b5dcf88db52
Victor Alvarez Pajaro <vicalpa@protonmail.com>
2018-12-07 01:23:23 -0500

    Remove width property from button styles

--- orange-aliens/src/components/EventCategories/EventCategoryCard.js
+++ orange-aliens/src/components/EventCategories/EventCategoryCard.js
@@ -6,20 +6,19 @@ import Typography from '@material-ui/core/Typography';
 import classNames from 'classnames';
 
 
+// some nice colors
 const colors = [
-    '#FF8C00', '#C8537B', '#6992C2', '#369939',
-    '#0C88C2', '#16623A', '#5C1E26', '#4A233',
+    '#FF8C00', '#C8537B', '#6992C', '#36993',
+    '#0C88C', '#16623A', '#5C1E26', '#4A233',
     '#800000', '#F94B1E', '#C8537B', '#FBEA86',
     '#C6162B', '#E4A42E', '#999333', '#937D62'
 ];
 
-
 const styles = {
     root: {
-        background: '#4A2339',
+        background: '#3F51B5',
         height: 130,
-        width: 280,
-        color: '#FFFFFF'
+        color: '#FFFFFF',
     }
 };
 
@@ -28,11 +27,10 @@ const EventCategoryCard = (props) => {
 
     return (
         <div>
-            <Button variant='contained' size='large' disableRipple={true} className={classNames(classes.root)} {...other}>
-                {/*<Typography variant='button' color='error'>
+            <Button variant='contained' fullWidth={true} disableRipple={true} className={classNames(classes.root)} {...other}>
+                <Typography variant='button' color='inherit'>
                     { props.categoryName }
-                </Typography> */}
-                { props.categoryName }
+                </Typography>
             </Button>
         </div>
     );
@@ -43,33 +41,3 @@ EventCategoryCard.propTypes = {
 }
 
 export default withStyles(styles)(EventCategoryCard);
-
-/*
-const styles = theme => ({
-    root: {
-        ...theme.mixins.gutters(),
-        paddingTop: theme.spacing.unit * 2,
-        paddingBottom: theme.spacing.unit * 2,
-    },
-});
-
-const EventCategoryCard = (props) => {
-    const { classes } = props;
-
-    return (
-        <div>
-            <Paper className={classes.root}>
-                <Typography variant='h6'>
-                    { props.eventName }
-                </Typography>
-            </Paper>
-        </div>
-    );
-}
-
-EventCategoryCard.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(EventCategoryCard);
-*/
