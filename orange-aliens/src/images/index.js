b1d4b32dbc2d2dd96888e6524dcf92610f8c9cc4
Rohan Swaby <rohan_swaby51@yahoo.com>
2018-12-13 09:49:53 -0500

    small fix

--- orange-aliens/src/images/index.js
+++ orange-aliens/src/images/index.js
@@ -5,4 +5,4 @@ export {default as newyork2} from './newyork2.jpg';
 export {default as newyork3} from './newyork3.png';
 export {default as newyork4} from './newyork4.jpg';
 export {default as newyork5} from './newyork5.jpg';
-export {default as accountCircle} from './accountCircle.png';
+//export {default as accountCircle} from './accountCircle.png';
