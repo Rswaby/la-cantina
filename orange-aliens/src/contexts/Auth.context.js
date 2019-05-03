a872711ff207fdaa464d3bffdd08b929d350804d
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-05-03 17:34:19 -0400

    clean up some more

--- orange-aliens/src/contexts/Auth.context.js
+++ orange-aliens/src/contexts/Auth.context.js
@@ -23,10 +23,11 @@ export class AuthProvider extends Component {
         })
     }
 
-    saveUserData(AuthToken, token){
+    saveUserData(AuthToken, token,user){
         localStorage.setItem(AuthToken,token);
         this.state({
-            token:token
+            token:token,
+            user: user
         })
     }
 
