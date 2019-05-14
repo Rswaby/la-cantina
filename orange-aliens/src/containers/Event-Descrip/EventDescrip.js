4cce33f8fd19d034e6abd31d68ca012994a6deee
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-05-14 15:58:27 -0400

    join event - by Victor Hong

--- orange-aliens/src/containers/Event-Descrip/EventDescrip.js
+++ orange-aliens/src/containers/Event-Descrip/EventDescrip.js
@@ -5,10 +5,17 @@ import { EventDescription } from '../../components'
 import { getEvent, getEventPhotos } from '../../fetches'
 
 class EventDescrip extends Component {
-  state = {
-    eventDetails: '',
-    eventPhtotos: '',
-    fetched: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventDetails: '',
+      eventPhotos: '',
+      fetched: false,
+      status: false,
+      attendCount: ''
+    }
+    this.handleJoinEvent = this.handleJoinEvent.bind(this);
+
   }
 
   componentDidMount() {
@@ -17,24 +24,41 @@ class EventDescrip extends Component {
 
     if (eventid && eventurl) {
       console.log("Inside event fetch")
-      getEvent(eventid,eventurl).then(data=>{
+      // get status from localStorage
+      const status = localStorage.getItem(eventid) ? localStorage.getItem(eventid) : false;
+      getEvent(eventid, eventurl).then(data => {
         this.setState({
-          eventDetails:data.response,
-          fetched:true
+          eventDetails: data.response,
+          fetched: true,
+          attendCount: data.response.yes_rsvp_count,
+          ...status
         })
       })
 
-      getEventPhotos(eventurl).then(data=>{
+      getEventPhotos(eventurl).then(data => {
         this.setState({
-          eventPhtotos:data.response
+          eventPhotos: data.response
         })
       })
 
     }
   }
+
+  handleJoinEvent() {
+    console.log("status: ", this.state.status);
+    let { attendCount } = this.state;
+    const status = localStorage.getItem(this.props.match.params.eventid);
+    attendCount = this.state.status ? attendCount - 1 : attendCount + 1
+    localStorage.setItem(this.props.match.params.eventid, !this.state.status);
+    this.setState({
+      status: !this.state.status,
+      attendCount:attendCount
+    })
+  }
+
   render() {
-    const { fetched, eventDetails} = this.state;
-    let day,DateTime;
+    const { fetched, eventDetails,attendCount } = this.state;
+    let day, DateTime;
     if (fetched && eventDetails) {
       day = eventDetails.local_date.split("-")
       DateTime = new Date(day[0], day[1] - 1, day[2])
@@ -43,14 +67,17 @@ class EventDescrip extends Component {
     return (
       <div>
         {fetched && eventDetails ? <EventDescription
-          address={eventDetails.venue ? eventDetails.venue.address_1 : "no address provided"  }
+          address={eventDetails.venue ? eventDetails.venue.address_1 : "no address provided"}
           attending={null}
           category={null}
-          capacity={eventDetails.yes_rsvp_count}
+          capacity={attendCount}
           DateTime={DateTime}
           description={eventDetails.description}
           name={eventDetails.name}
           organizer={null}
+          handleJoinEvent={this.handleJoinEvent}
+          //eventid={this.props.match.params.eventid}
+          status={this.state.status}
         /> : <div>loading ...</div>
         }
       </div>
